e699b41171a904ff88773a03c9a2997f7710d641
malize <cfredmalize@gmail.com>
2023-01-23 13:38:50 -0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,63 @@
+let closeMenu=document.querySelector('#menu')
+let nav=document.querySelector('.nav-links')
+closeMenu.addEventListener('click',function(){
+ nav.style.right="0"
+ console.log("you are badass dev")
+})
+let openMenu=document.querySelector('#times')
+openMenu.addEventListener('click',function(){
+ nav.style.right="-200px"
+ console.log("you are badass dev")
+})
+
+// let cars=["honda","toyota","ford"]
+// for(let i=0;i<cars.length;i++){
+//     if(cars[i]==="honda"){
+//         cars[i]="mitshibushi";
+       
+//     }
+//     console.log(cars[i])
+// }
+// class user{
+//     constructor(name,age){
+//         this.name=name;
+//         this.age=age
+      
+//     }
+   
+// }
+// const person =new user("amara",33)
+// delete person.name
+// person.state="imo"
+// person.about=myname()
+// console.log(person)
+// function myname(){
+//     return ("i am young")
+// }
+// /*javascript toturials for today*/
+// // let totalNum=0;
+// // let message;
+// // if(totalNum<0){
+// //     if(totalNum%2===0){
+// //         console.log(message)
+// //     }
+// // }
+// // console.log(totalNum)
+
+// for(let i=0; i<10; i++){
+//   let num=i;
+//     if(num==5){
+//       let name="you are too good to fail me lord"
+//      console.log( num=name);
+//     }
+//     else{
+//         console.log(i
+//             )
+//     }
+  
+  
+// }
+
+Window.prompt("what are you saying");
+let wordsCounting=words.lenght;
+console.log(wordsCounting)
